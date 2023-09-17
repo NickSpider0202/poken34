deae39620279f911f183ce66f1b766536c2b13ce
NickS <nicolasrexona@gmail.com>
2023-09-17 15:56:27 -0300

    update_01

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,9 @@ const limit = 10
 const url ='https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
 
 
-fetch(url)
+fetch(url).then(function (response){
+    console.log(response)
+})
+
+const x = 10 + 10
+console.log(x)
