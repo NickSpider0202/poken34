7c4ee4abe8dcf5b0f0469ca46acd7b0d4840bd1b
NickS <nicolasrexona@gmail.com>
2023-09-17 19:46:15 -0300

    update_2

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,50 @@
 
 const offset = 0
 const limit = 10
-const url ='https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+function convertPokemonToLi(pokemon) {
+    return `
+    <li class="pokemon">
+    <span class="number">#001</span>
+    <span class="name">${pokemon.name}</span>
 
-fetch(url).then(function (response){
-    console.log(response)
-})
+   <div class="detail">
+    <ol class="types">
+            <li class="type">grass</li>
+            <li class="type">poison</li>
+    </ol>
+    
+     <img src="https://gifs.eco.br/wp-content/uploads/2023/07/imagens-do-bulbasaur-png-0.png" 
+        alt="${pokemon.name}">
+   
+    </div>
+</li>
+    
+    `
+
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+       
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            console.log()  
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+        }
+            
+        
+    })
+
+    .catch((error) => console.error(error))
+    // .finally (function () {
+    //     console.log('Requisiçao concluida!')
+    // })
 
 const x = 10 + 10
 console.log(x)
